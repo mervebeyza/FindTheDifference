70c27a48ead7936d43ac40d534ff3a06ef81a70f
mervebeyza <karatasmervebeyza@gmail.com>
2022-08-23 11:28:04 +0300

    Find The Difference
    Find The Difference

---
+++ Assets/Scripts/level8.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class level8 : MonoBehaviour
+{
+
+    public Button owlEyes;
+    public Button owlEyes2;
+
+    Text score;
+    
+}
